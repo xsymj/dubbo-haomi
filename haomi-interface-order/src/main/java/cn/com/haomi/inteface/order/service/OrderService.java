9803257f48eeafb3012404a1357f716f0e88a9b6
徐思源 <mhfand444@163.com>
2017-08-29 01:30:21 +0800

    初始化

---
+++ haomi-interface-order/src/main/java/cn/com/haomi/inteface/order/service/OrderService.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+/**
+ * @author xusiyuan
+ *
+ */
+package cn.com.haomi.inteface.order.service;
+
+import java.util.List;
+import java.util.Map;
+
+public interface OrderService{
+	
+		public List<Map<String, Object>> list();
+}
---
+++ haomi-service-order/src/main/java/cn/com/haomi/order/mapper/OrderMapper.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+/**
+ * @author xusiyuan
+ *
+ */
+package cn.com.haomi.order.mapper;
+
+import java.util.Map;
+
+import java.util.List;
+
+
+public interface OrderMapper{
+	
+		List<Map<String, Object>> list();
+}
